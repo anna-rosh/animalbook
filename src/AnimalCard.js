c6405cf84603a081c8d876a57bc51ca8fd90fcb8
Anna Roshchina <annie.roshchina@yahoo.com>
2020-10-10 20:30:10 +0200

    sound added

--- src/AnimalCard.js
+++ src/AnimalCard.js
@@ -16,8 +16,8 @@ export default function AnimalCard() {
 
     }, []);
 
-    const playAudio = async () => {
-        let audioEl = document.getElementsByClassName('term-audio')[0];
+    const playAudio = async (elementClass) => {
+        let audioEl = document.getElementsByClassName(elementClass)[0];
         await audioEl.play();
         // console.log('the button was clicked', elementClass);
     };
@@ -30,11 +30,15 @@ export default function AnimalCard() {
     return (
         <section className="animal-card-container">
             <div className="card-img-container">
-                <img src={clickedAnimal.img} alt={clickedAnimal.animal}/>
+                <img src={clickedAnimal.img} alt={clickedAnimal.animal} />
             </div>
-            <button onClick={playAudio}>play term</button>
-            <audio className='term-audio'>
+            <button onClick={() => playAudio("term-audio")}>play term</button>
+            <audio className="term-audio">
                 <source src={clickedAnimal.term_read}></source>
+            </audio>
+            <button onClick={() => playAudio("sound-audio")}>play sound</button>
+            <audio className="sound-audio">
+                <source src={clickedAnimal.sound}></source>
             </audio>
         </section>
     );
