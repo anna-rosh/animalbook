5585120fe5b3eaab05471eabbbdb0f861258b3eb
Anna Roshchina <annie.roshchina@yahoo.com>
2020-10-31 15:30:30 +0100

    quiz: refactoring in progress

--- src/Question.js
+++ src/Question.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { getQuizQuestion, updateQuestionCount, updateUserScore } from './actions';
+import { getQuizQuestion, updateUserScore } from './actions';
 import { playAudio } from './play';
 import { ThumbsUp, MessageCircle } from 'react-feather';
 
@@ -11,18 +11,17 @@ export default function Question() {
 
     useEffect(() => {
         dispatch(getQuizQuestion());
-        dispatch(updateQuestionCount(0));
 
     }, []);
 
-    const checkAnswer = (clickedAnswer) => {
-        if (clickedAnswer === quizQuestion.correctAnswer) {
+    const checkAnswer = (clickedAnswerId) => {
+        if (clickedAnswerId === quizQuestion.question.id) {
             dispatch(updateUserScore(1));
-            playRightAnswer(quizQuestion.correctAnswer, 'term-audio');
+            playRightAnswer(quizQuestion.question.id);
             ShowNextQuestion(dispatch, questionCount);   
         } else {
             dispatch(updateUserScore(2));
-            playRightAnswer(quizQuestion.correctAnswer, "term-audio");
+            playRightAnswer(quizQuestion.question.id);
             ShowNextQuestion(dispatch, questionCount);
         }
     };
@@ -44,15 +43,15 @@ export default function Question() {
                 {quizQuestion.answers.map((answer, i) => {
                     return (
                         <div className="answer" key={i}>
-                            <div className="audio-btn-quiz" onClick={() => playAudio("term-audio", i)}>
+                            <div className="audio-btn-quiz" id={answer.id} onClick={() => playAudio("term-audio", i)}>
                                 <MessageCircle className="message-circle mirrored" />
                                 <img className="audio-btn-img" src="/img/person-speaks.png" />
                             </div>
-                            <div className="thumb-container" onClick={() => checkAnswer(answer)}>
+                            <div className="thumb-container" onClick={() => checkAnswer(answer.id)}>
                                 <ThumbsUp className="thumbs-up" /> 
                             </div>
-                            <audio className="term-audio">
-                                <source  src={answer}></source>
+                            <audio className={`term-audio ${answer.id}`}>
+                                <source  src={answer.term_read}></source>
                             </audio>
                         </div>
                     );
@@ -65,7 +64,6 @@ export default function Question() {
 const ShowNextQuestion = (dispatch, questionCount) => {
     setTimeout(() => {
         let currVal = questionCount;
-        dispatch(updateQuestionCount(currVal));
         dispatch(getQuizQuestion());
         let btns = document.getElementsByClassName("audio-btn-quiz");
 
@@ -75,24 +73,10 @@ const ShowNextQuestion = (dispatch, questionCount) => {
     }, 2500);
 };
 
-const playRightAnswer = (correctAnswer, elementClass) => {
-    // get a collection of audio elements
-    const allAudios = document.getElementsByClassName(elementClass);
-    // make the collection to an array to me able to get the index of the audio
-    // with the right answer
-    let allAudiosArr = [];
-    let rightAudio;
-    // loop over the elemet arr to find the element containing the right answer
-    for (let i = 0; i < allAudios.length; i++) {
-        if (allAudios[i].innerHTML.includes(correctAnswer)) {
-            rightAudio = allAudios[i];
-        }
-        allAudiosArr.push(allAudios[i]);
-    }
-    // find out the index of the element with the right audio
-    let rightAudioIndex = allAudiosArr.indexOf(rightAudio);
-    // display that this answer is correct
-    document.getElementsByClassName("audio-btn-quiz")[rightAudioIndex].classList.add("green-bg");
-    // play the correct audio
-    playAudio(elementClass, rightAudioIndex);
+const playRightAnswer = (correctAnswerId) => {
+    // get the right btn by Id which corresponds this the id of the right answer
+    const rightOption = document.getElementById(`${correctAnswerId}`);
+    rightOption.classList.add("green-bg");
+
+    playAudio(`${correctAnswerId}`, 0);
 };
